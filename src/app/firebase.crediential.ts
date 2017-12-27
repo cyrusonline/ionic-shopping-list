317c216eef1f647dde8c7b3cf5e77b3b517e87b9
cyrus <online_mad@yahoo.com.hk>
2017-12-27 11:07:46 +0800

    firt commit

---
+++ src/app/firebase.crediential.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = 
+    {
+    apiKey: "AIzaSyD_F6wadHnsR9skYzSg387EGihzy8HLpL8",
+    authDomain: "angularfire-f5132.firebaseapp.com",
+    databaseURL: "https://angularfire-f5132.firebaseio.com",
+    projectId: "angularfire-f5132",
+    storageBucket: "",
+    messagingSenderId: "21716328556"
+  }
